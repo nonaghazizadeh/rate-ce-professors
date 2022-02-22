d79c33f94adf830c1389cf27a6a29c13b9c9ab59
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-02-23 01:06:40 +0330

    feat: add register view for signup component

---
+++ front/src/views/Register.vue
@@ -0,0 +1,13 @@
+<template>
+  <sign-up />
+</template>
+
+<script>
+import SignUp from "../components/SignUp.vue";
+export default {
+  name: "Register",
+  components: { SignUp },
+};
+</script>
+
+<style></style>
