e255684cfdefa8767db745f1ab26ef90193ff8cd
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-02-23 01:09:02 +0330

    feat: add professor info view

---
+++ front/src/views/ProfInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <prof />
+</template>
+
+<script>
+import Prof from "../components/Prof.vue";
+export default {
+  components: { Prof },
+  name: "ProfInfo",
+};
+</script>
+
+<style></style>
