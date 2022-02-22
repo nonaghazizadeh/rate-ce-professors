3fafb201ca5cf5eabe4c60113d758430c8867251
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-02-23 01:11:24 +0330

    feat: add user view for user info page

---
+++ front/src/views/User.vue
@@ -0,0 +1,13 @@
+<template>
+  <user />
+</template>
+
+<script>
+import User from "../components/User.vue";
+export default {
+  name: "Register",
+  components: { User },
+};
+</script>
+
+<style></style>
